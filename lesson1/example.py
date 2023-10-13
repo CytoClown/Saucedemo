4d886533995c61a0c398c64d05f09c0b1c45ad78
CytoClovn <nickolayknz@gmail.com>
2023-10-13 15:36:28 +0300

    unversioned files changed

---
+++ lesson1/example.py
@@ -0,0 +1,5 @@
+data = ['Test.allTheThings() T-Shirt (Red)', 'Sauce Labs Onesie', 'Sauce Labs Fleece Jacket', 'Sauce Labs Bolt T-Shirt', 'Sauce Labs Bike Light', 'Sauce Labs Backpack']
+# data1 = list(reversed(sorted(data)))
+# print(data1)
+# print(data)
+print(data == list(reversed(sorted(data))))
